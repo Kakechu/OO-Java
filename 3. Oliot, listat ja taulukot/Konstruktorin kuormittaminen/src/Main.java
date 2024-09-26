694cfbb1f27253422e7d99fc434240c7bdc47451
Kaisa Leinonen <laurakaisaleinonen@gmail.com>
2024-09-26 22:24:26 +0300

    konstruktoritehtävän viimeistelyä

--- 3. Oliot, listat ja taulukot/Konstruktorin kuormittaminen/src/Main.java
+++ 3. Oliot, listat ja taulukot/Konstruktorin kuormittaminen/src/Main.java
@@ -1,12 +1,20 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Unilelu> lelut = new ArrayList<>();
 
         Unilelu lelu1 = new Unilelu("Pupu", "pupu", 1);
+        lelut.add(lelu1);
         Unilelu lelu2 = new Unilelu("Pupu");
+        lelut.add(lelu2);
         Unilelu lelu3 = new Unilelu("Pupu", "pupu");
+        lelut.add(lelu3);
+        Unilelu lelu4 = new Unilelu("Topi", "nalle");
+        lelut.add(lelu4);
 
-        System.out.println(lelu1);
-        System.out.println(lelu2);
-        System.out.println(lelu3);
+        for(Unilelu lelu : lelut) {
+            System.out.println(lelu);
+        }
     }
 }
